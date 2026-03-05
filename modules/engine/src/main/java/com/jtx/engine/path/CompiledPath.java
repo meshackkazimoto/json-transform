c1134740466781004c5cbfc0502eb32dcb9aaa46
meshackkazimoto <meshackkazimoto@gmail.com>
2026-03-05 22:58:44 +0300

    testing and modules

--- modules/engine/src/main/java/com/jtx/engine/path/CompiledPath.java
+++ modules/engine/src/main/java/com/jtx/engine/path/CompiledPath.java
@@ -1,5 +1,22 @@
 package com.jtx.engine.path;
 
+/*
+ * Copyright (C) 2026 Meshack Kazimoto
+ *
+ * JTX (JSON Transform) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JTX (JSON Transform) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JTX (JSON Transform).  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 
